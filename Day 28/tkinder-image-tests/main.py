465354457a215eaeb21047b151c0e590fa5147df
Cinar Okutan <110976036+Ninj0re@users.noreply.github.com>
2024-03-22 00:14:30 +0300

    Day 28

---
+++ Day 28/tkinder-image-tests/main.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+from PIL import ImageTk, Image
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+
+#This creates the main window of an application
+window = tk.Tk()
+window.title("Join")
+window.geometry("623x348")
+window.configure(background='grey')
+
+path = "forest.jpg"
+
+#Creates a Tkinter-compatible photo image, which can be used everywhere Tkinter expects an image object.
+img = ImageTk.PhotoImage(Image.open(path))
+
+#The Label widget is a standard Tkinter widget used to display a text or image on the screen.
+panel = tk.Label(window, image = img)
+
+#The Pack geometry manager packs widgets in rows or columns.
+panel.pack(side = "bottom", fill = "both", expand = "yes")
+
+#Start the GUI
+window.mainloop()
